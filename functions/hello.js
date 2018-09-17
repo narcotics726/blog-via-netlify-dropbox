f00854533734b88c3de1b2ffbaf204a21dbe04dd
narcotics726 <narcotics726@gmail.com>
2018-09-18 00:06:50 +0800

    figure out the cwd of lambda

--- functions/hello.js
+++ functions/hello.js
@@ -5,22 +5,21 @@ const fs = require('fs');
 const childProcess = require('child_process');
 
 exports.handler = function (event, context, callback) {
-    console.log(event);
-    console.log(context);
-    fs.readdir('./source/_posts', (err, files) => {
+    console.log(`cwd: ${process.cwd()}`);
+    fs.readdir('../', (err, files) => {
         if (err) {
             return callback(err, {statusCode: 500});
         }
 
-        fs.writeFile('./source/_posts/test.md', '#Test Blog From Function', (err) => {
-            if (err) {
-                return callback(err);
-            }
+        // fs.writeFile('./source/_posts/test.md', '#Test Blog From Function', (err) => {
+        //     if (err) {
+        //         return callback(err);
+        //     }
 
-            childProcess.execSync('./node_modules/.bin/hexo generate');
+        //     childProcess.execSync('./node_modules/.bin/hexo generate');
 
             return callback(null, { statusCode: 200, body: JSON.stringify(files) });
-        })
+        // })
 
     });
 };
